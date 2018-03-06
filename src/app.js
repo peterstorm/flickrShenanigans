27399501b713b78303582a236383b250a333ae57
peterstorm <peterstorm@peterstorms-Mac-Pro.local>
2018-03-06 21:50:23 +0100

    add typesignatures

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const request = require('request')
 const { compose, curry, path, pipe, map, prop } = require('ramda')
 
 // HELPER FUNCTIONS
+const url = 'https://api.flickr.com/services/feeds/photos_public.gne/?tags=aww&format=json&nojsoncallback=1'
 
 const trace = x => { console.log(x); return x }
 
@@ -20,33 +21,36 @@ const parse = Either.try(JSON.parse)
 const eitherToTask = e =>
   e.fold(Task.rejected, Task.of)
 
-// getJSON ::
+// getJSON :: Url = Task Error JSON
 const getJSON = url =>
   httpGet(url)
     .map(parse)
     .chain(eitherToTask)
 
-const url = 'https://api.flickr.com/services/feeds/photos_public.gne/?tags=aww&format=json&nojsoncallback=1'
+// APP
 
-
-
+// extractPhotos :: JSON -> [Photos]
 const extractPhotos = compose(map(prop('m')), map(prop('media')) ,prop('items'))
 
+// photos :: JSON -> [Photos]
 const photos  = compose(map(extractPhotos) ,getJSON)
 
+// img :: Url -> IMG tag
 const img = x => {
   let el = document.createElement('img');
   el.setAttribute('src', x);
   return el;
 }
 
+// makeImgs :: [Photos] -> [imgs]
 const makeImgs = compose(map(map(img)), photos)
 
 const getElement = x => document.getElementById(x)
 
+// setHtml :: [Photos] -> _
 const setHtml = x => getElement('js-main').append(x)
 
-
+// [Photos] -> DOM
 makeImgs(url).fork(console.err, map(setHtml) )
 
 
