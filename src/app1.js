6d6ecf7bca892daac3ca6e5632389c2312733600
Peter Storm <peterstorm@Peters-MacBook-Pro.local>
2018-03-11 23:05:35 +0100

    implement Sanctuary solution

--- src/app1.js
+++ src/app1.js
@@ -1,26 +1,58 @@
-const { task } = require('folktale/concurrency/task')
+const { result, concurrency: { task } } = require('folktale')
 const request = require('request')
 const { compose, map, prop } = require('ramda')
+const url = 'https://api.flickr.com/services/feeds/photos_public.gne/?tags=aww&format=json&nojsoncallback=1'
 
-const requestUrl = function(url) {
-  return task(resolver => {
-      request(url, (err, res, body) => {
-            err ? resolver.reject(err) : resolver.resolve(body);
-          })
-   })
+const safeParse = json => result.try(_ => JSON.parse(json))
+
+const resultToTask = result => {
+  return result.matchWith({
+    Ok: ({ value }) => task.of(value),
+    Error: ({ value }) => task.rejected(value)
+  })
 }
 
-const call = requestUrl('https://api.flickr.com/services/feeds/photos_public.gne/?tags=cats&format=json&jsoncallback=?')
+const getUrl = task.fromNodeback(request)
 
-const url = 'https://api.flickr.com/services/feeds/photos_public.gne/?tags=cats&format=json&jsoncallback=?'
-//call.run().listen({
-//  onCancelled: () => { console.log('the task was cancelled') },
-//    onRejected: (error) => { console.log('something went wrong') },
-//    onResolved: (value) => { console.log(`The value is ${value}`) }
-//});
+const responseBody = response => response.body
 
-const result = requestUrl(url).map(prop('items'))
+const flickrItems =
+  getUrl(url)
+    .map(responseBody)
+    .chain(x => resultToTask(safeParse(x)))
 
-result.run().listen({
+const results = compose(map(prop('items')), flickrItems)
+
+results.run().listen({
   onResolved: x => console.log(x)
 })
+// extractPhotos :: JSON -> [Photos]
+//const extractPhotos = compose(map(prop('m')), map(prop('media')) ,prop('items'))
+
+// photos :: JSON -> [Photos]
+//const photos = compose(map(extractPhotos), flickrItems)
+//
+//// img :: Url -> IMG tag
+//const img = x => {
+//  let el = document.createElement('img');
+//  el.setAttribute('src', x);
+//  return el;
+//}
+//
+//// makeImgs :: [Photos] -> [imgs]
+//const makeImgs = compose(map(map(img)), photos)
+//
+//const getElement = x => document.getElementById(x)
+//
+//// setHtml :: [Photos] -> _
+//const setHtml = x => getElement('js-main').append(x)
+//
+//// [Photos] -> DOM
+//makeImgs(url).run().listen({
+//  onCancelled: _ => console.log('task was cancelled',
+//  onRejected: error => console.log(error),
+//  onResolved: x => map(setHtml,x),
+//})
+
+
+
