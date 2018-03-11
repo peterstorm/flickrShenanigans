30280069c349e04e07b26c957760e6c5cdbf22f3
Peter Storm <peterstorm@Peters-MacBook-Pro.local>
2018-03-12 00:04:41 +0100

    implement Folktale 2.0 solution

--- src/app1.js
+++ src/app1.js
@@ -21,38 +21,34 @@ const flickrItems =
     .map(responseBody)
     .chain(x => resultToTask(safeParse(x)))
 
-const results = compose(map(prop('items')), flickrItems)
+const extractPhotos = compose(map(prop('m')), map(prop('media')),prop('items'))
 
-results.run().listen({
-  onResolved: x => console.log(x)
+const getPhotos  = flickrItems
+                    .map(extractPhotos)
+
+// img :: Url -> IMG tag
+const img = x => {
+  let el = document.createElement('img');
+  el.setAttribute('src', x);
+  return el;
+}
+
+// makeImgs :: [Photos] -> [imgs]
+const makeImgs = getPhotos
+                  .map(map(img))
+
+
+const getElement = x => document.getElementById(x)
+
+// setHtml :: [Photos] -> _
+const setHtml = x => getElement('js-main').append(x)
+
+// [Photos] -> DOM
+makeImgs.run().listen({
+  onCancelled: _ => console.log('task was cancelled'),
+  onRejected: error => console.log(error),
+  onResolved: x => map(setHtml,x),
 })
-// extractPhotos :: JSON -> [Photos]
-//const extractPhotos = compose(map(prop('m')), map(prop('media')) ,prop('items'))
-
-// photos :: JSON -> [Photos]
-//const photos = compose(map(extractPhotos), flickrItems)
-//
-//// img :: Url -> IMG tag
-//const img = x => {
-//  let el = document.createElement('img');
-//  el.setAttribute('src', x);
-//  return el;
-//}
-//
-//// makeImgs :: [Photos] -> [imgs]
-//const makeImgs = compose(map(map(img)), photos)
-//
-//const getElement = x => document.getElementById(x)
-//
-//// setHtml :: [Photos] -> _
-//const setHtml = x => getElement('js-main').append(x)
-//
-//// [Photos] -> DOM
-//makeImgs(url).run().listen({
-//  onCancelled: _ => console.log('task was cancelled',
-//  onRejected: error => console.log(error),
-//  onResolved: x => map(setHtml,x),
-//})
 
 
 
